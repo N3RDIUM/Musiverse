f67d06329e2d2cdc209e417b679f2e8aaffec948
n3rdium <n3rdium@gmail.com>
2024-06-07 23:40:44 +0530

    Better scrolling on library

--- screens/library.py
+++ screens/library.py
@@ -31,6 +31,7 @@ class Library(Screen):
         self.cursor_position = 0
         self.view_position = 0
         self.select = 0
+        self.pos = 0
         
     def start_search(self):
         self.manager = Manager()
@@ -82,30 +83,35 @@ class Library(Screen):
             query = self.namespace.query[self.view_position : self.view_position + w - 2]
             cursor_position = self.cursor_position - self.view_position
             
-            if cursor_position > len(query):
-                self.cursor_position = len(query) + self.view_position
-            if cursor_position < 1:
-                self.cursor_position = 0
-                
-            if cursor_position > w - 3:
-                self.view_position = len(self.query) - (w - 3)
-            if cursor_position == 1 and self.view_position > 0:
-                self.view_position -= 1
-            
             render[0] = "╭" + "─" * (w - 2) + "╮"
             render[1] = "│" + query + " " * (w - 2 - len(query)) + "│"
             render[2] = "╰" + "─" * (w - 2) + "╯"
             
             for x, row in enumerate(render):
                 stdscr.addstr(x, 0, ''.join(row), color_pair(DEFAULT))
+            
+            if self.select > h - self.app.props['statusbar'].height and self.pos < len(self.namespace.results):
+                self.select = h - self.app.props['statusbar'].height
+                self.pos += 1
+            if self.select < 1 and self.pos > 0:
+                self.select = 1
+                self.pos -= 1
                 
-            for i, result in enumerate(self.namespace.results):
-                if i + self.view_position >= h - self.app.props['statusbar'].height:
-                    break
+            if self.select + self.pos >= len(self.namespace.results):
+                self.select -= 1
+            if self.select + self.pos < 0:
+                self.select += 1
+            
+            results = self.namespace.results
+            for i, result in enumerate(results):
+                i -= self.pos
+                if i < 0 or i >= len(results): continue
+                if i > h - self.app.props['statusbar'].height: continue
                 rendered = result.render(w - 3)
                 pair = SELECTED if i == self.select else DEFAULT
                 cursor = " " if i != self.select else ("" if time() % config['cursor_blink_rate'] < 0.5 else " ")
                 stdscr.addstr(i + 3, 1, cursor + " " + rendered +  " " * (w - 4 - len(rendered)), color_pair(pair))
+                
                 if i == self.select: self.app.props['library_selected'] = result
             
             if len(self.namespace.results) == 0:
@@ -116,8 +122,7 @@ class Library(Screen):
                 
             # Cursor
             stdscr.addstr(1, cursor_position + 1, ("│" if (time() + 0.5) % config['cursor_blink_rate'] < 0.5 else " "), color_pair(CURSOR))
-        except:
-            print(f'Could not render!')
+        except: pass
 
     def handle_key(self, ch: int, stdscr: window):
         if ch in ALLOWED:
@@ -139,16 +144,14 @@ class Library(Screen):
         elif ch == KEY_UP:
             self.select -= 1
             if self.select < 0:
-                self.select = len(self.namespace.results) - 1
+                self.select = 0
         elif ch == KEY_DOWN:
             self.select += 1
-            if self.select >= len(self.namespace.results):
-                self.select = 0
         elif ch == KEY_ENTER:
             pass # Create playlist if it doesn't exist
         return True
     
     def on_navigate(self):
         self.app.props['keylock'] = True
-        self.app.props['keybinds'] = '[󱊷] Back [󰌑] Download'
+        self.app.props['keybinds'] = '[󱊷] Back [󰌑] Create'
         self.start_search()
